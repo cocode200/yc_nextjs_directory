7299d12c95923ced90f1cf2c262d81890916f805
cocode200 <tran.cocode@gmail.com>
2025-03-17 12:48:26 -0700

    update: implemented the correct next-auth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div>
-      <h1 className="text-2xl">
+      <h1 className="heading font-work-sans">
         HOME
       </h1>
     </div>
